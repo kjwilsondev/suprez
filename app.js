209db4864db27dc0448c704800a9dd88185c4749
sadboyKJ <yourboykjdev@gmail.com>
2019-02-24 13:59:47 -0800

    thisll work

--- app.js
+++ app.js
@@ -1,62 +1,38 @@
-// middleware
-const path         = require('path');        // utilities for working with directory paths
-const createError  = require('http-errors'); // error handling
-const logger       = require('morgan');      // logs requests
-const bodyParser   = require('body-parser'); // body parsing
+var createError  = require('http-errors');
+var express      = require('express');
+var exphbs       = require('express-handlebars');
+var path         = require('path');
+var cookieParser = require('cookie-parser');
+var logger       = require('morgan');
+var dotenv       = require('dotenv').config();
 
-// express
-const express      = require('express');
-const exphbs       = require('express-handlebars');
-
-// sessions
-const cookieParser = require('cookie-parser');
-// const session      = require('express-session')
-
-// apis
-const dotenv       = require('dotenv').config();
-// const passport     = require('./config/passport');
 
 // routes
-const indexRouter  = require('./routes/index');
-const usersRouter  = require('./routes/users');
-const giphyRouter  = require('./routes/giph');
+var indexRouter  = require('./routes/index');
+var usersRouter  = require('./routes/users');
+var giphyRouter  = require('./routes/giph');
 
 var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
 // configure sessions
 const session = require('express-session');
-// app.use(session({ secret: 'secret-unique-code', cookie: { maxAge: 3600000 }, resave: true, saveUninitialized: true }));
+app.use(session({ secret: 'secret-unique-code', cookie: { maxAge: 3600000 }, resave: true, saveUninitialized: true }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// middleware activation
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cookieParser());
-app.use(session({
-    secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 // routers
-app.use('/',      indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/giph',  giphyRouter);
+app.use('/giph', giphyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -82,7 +58,5 @@ mongoose.connect(mongoURI)
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-require("./routes/api.js")(app);
 
 module.exports = app;
